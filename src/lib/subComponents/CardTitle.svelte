eff7710f908994bbf6fc2ff5f5809685a531f50c
dragonflyinmypool <jheiney10@gmail.com>
2023-03-06 15:16:56 -0500

    changed graphic design

--- src/lib/subComponents/CardTitle.svelte
+++ src/lib/subComponents/CardTitle.svelte
@@ -1,19 +1,25 @@
 <script>
-	import StatusBar from './StatusBar.svelte';
-	export let data;
+  import StatusBar from './StatusBar.svelte';
+  export let data;
 </script>
 
 <div>
-	<h3>
-		{data.title}
-	</h3>
-	<StatusBar statusData={data.status} />
+  <h3>
+    {data.title}
+  </h3>
+  <StatusBar statusData={data.status} />
 </div>
 
 <style>
-	div {
-		display: flex;
-		align-items: center;
-		gap: 0.5rem;
-	}
+  h3 {
+    font-size: 2.25rem;
+    font-weight: 500;
+    margin: 0;
+    font-family: myFirstFont;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
 </style>
